6538f8f0258f3b06dfc7177f1845bacc137d606b
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-09-14 11:08:23 -0400

    View complete added MainCtrl for view

--- app/app.js
+++ app/app.js
@@ -2,11 +2,22 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'MainCtrl'
+        })
+            .otherwise({redirectTo: '/'});
+    }])
+
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.display = '1337';
+        var operandOne = 0.0;
+        var operandTwe = 0.0;
+        var input = '';
+
+    }]);
